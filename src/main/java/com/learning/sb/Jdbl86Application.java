bb6f4a8c5e7379f1354f05db36a8cf0542931725
Jinesh <jineshsonawat9929@gmail.com>
2026-02-17 00:15:01 +0530

    Initialize Spring Boot Project

---
+++ src/main/java/com/learning/sb/Jdbl86Application.java
@@ -0,0 +1,13 @@
+package com.learning.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdbl86Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jdbl86Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/sb/Jdbl86ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdbl86ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
