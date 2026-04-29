be4e8a2ec3bd0883ab307d9698946aa59b7f6b79
Jinesh <jineshsonawat9929@gmail.com>
2026-04-29 17:34:59 +0530

    Showing how can we use one connection for all.

--- src/main/java/com/learning/sb/jdbc/JDBCDemoOfUsingSingleConnection.java
+++ src/main/java/com/learning/sb/jdbc/JDBCDemoOfUsingSingleConnection.java
@@ -5,40 +5,40 @@ import com.learning.sb.model.Employee;
 
 import java.sql.*;
 
-public class JDBCConnectionDemo {
+public class JDBCDemoOfUsingSingleConnection {
 
+    private static final Connection connection = MyDBConnection.getConnection();
 
-    public static final String url = "jdbc:postgresql://localhost:8080/postgres";
-    public static final String user = "postgres";
-    public static final String password = "1101";
 
     public static void main(String[] args) throws SQLException {
 
-
-        String createCommand = "CREATE TABLE employee ("
-                + "id SERIAL PRIMARY KEY, "
-                + "name VARCHAR(100), "
-                + "department VARCHAR(100))";
-        String selectCommand = "Select * from public.employee";
+        try {
+            String createCommand = "CREATE TABLE employee ("
+                    + "id SERIAL PRIMARY KEY, "
+                    + "name VARCHAR(100), "
+                    + "department VARCHAR(100))";
+            String selectCommand = "Select * from public.employee";
 
 //        runUpdate(createCommand);
-//        addEmployee(new Employee("Dharmesh", "IOS"));
-        deleteEmployeeByName(new Employee("Dharmesh"));
+            addEmployee(new Employee("Dharmesh", "IOS"));
+            addEmployee(new Employee("Janak", "Design"));
+            deleteEmployeeByName(new Employee("Jinesh"));
 
-        ResultSet r = runQuery(selectCommand);
-        while (r != null && r.next()) {
-            System.out.println(r.getString("name") + "-" + r.getString("department"));
+
+            ResultSet r = runQuery(selectCommand);
+            while (r != null && r.next()) {
+                System.out.println(r.getString("name") + "-" + r.getString("department"));
+            }
+        }finally{
+            connection.close();;
         }
 
     }
 
     public static void addEmployee(Employee e) {
-//        This is so hard to write so we can use Prepared Statement to get rid of this.
-//        String insertCommand = "INSERT INTO employee (name, department) VALUES ('"
-//                + e.getName() + "', '"
-//                + e.getDepartment() + "')";
+
         String insertCommand = "INSERT INTO employee (name, department) VALUES (?, ?)";
-        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+        try {
             try (PreparedStatement preparedStatement = connection.prepareStatement(insertCommand)) {
 
                 preparedStatement.setString(1, e.getName());
@@ -58,7 +58,7 @@ public class JDBCConnectionDemo {
     public static void deleteEmployeeByName(Employee e) {
         String deleteCommand = "DELETE FROM employee WHERE name = ?";
 
-        try (Connection connection = DriverManager.getConnection(url,user,password)) {
+        try {
             try (PreparedStatement preparedStatement = connection.prepareStatement(deleteCommand)) {
                 preparedStatement.setString(1, e.getName());
 
@@ -71,15 +71,12 @@ public class JDBCConnectionDemo {
     }
 
     public static void runUpdate(String query) {
-        //        try() - try with resources
-        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+        try {
             Statement statement = connection.createStatement();
             statement.executeUpdate(query);
             System.out.println("Connected to Database");
 
-//            We need to close the connection that we open to connect with database. but we also can use try with resources where we can put the connection in () that we want to close and no need to write the close().
-//            No need to this now
-//            connection.close();
+
         } catch (SQLException e) {
             System.out.println("Connection Failed");
             System.out.println(e.getMessage());
@@ -87,7 +84,7 @@ public class JDBCConnectionDemo {
     }
 
     public static ResultSet runQuery(String query) {
-        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+        try {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(query);
 
